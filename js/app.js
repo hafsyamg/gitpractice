89e3d08cd6803875217bf61a8a3e329b2db13ace
Hafsah <hafsyamg@gmail.com>
2023-10-05 15:53:22 +0100

    all done

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// let x = document.getElementById("learn")
+// x.innerHTML="learn Something for the future";
+
+// let m= document.querySelectorAll(".add-to-cart")
+// for(x=0; x< m.length; x++){
+// console.log(m[x].innerHTML= "BUY NOW");
+// }
+
+let x = document.querySelectorAll(".info-card p")
+x.innerHTML="Hafsa";
